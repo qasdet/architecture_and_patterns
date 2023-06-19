77960872a37dfb11dc05989b244debb61e708d0d
qasdet <qa-admin@sknt.ru>
2023-06-19 14:01:20 +0300

    add README.MD

--- lesson_02/framework/templator.py
+++ lesson_02/framework/templator.py
@@ -3,7 +3,7 @@ from jinja2 import Template
 
 
 def render(template_name, **kwargs):
-    file_path = join('templates', template_name)
+    file_path = join('./templates', template_name)
     with open(file_path, encoding='utf-8') as f:
         template = Template(f.read())
     return template.render(**kwargs)
--- lesson_02/run.py
+++ lesson_02/run.py
@@ -1,4 +1,10 @@
 from urls import routes
 from framework.main import Framework
+from wsgiref.simple_server import make_server
 
 app = Framework(routes)
+
+
+with make_server('', 8080, app) as httpd:
+    print("Запуск на порту 8080...")
+    httpd.serve_forever()
