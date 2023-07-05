241a38ef821ae8b1def33307be7e06f13c90aac3
qasdet <qa-admin@sknt.ru>
2023-07-06 01:05:54 +0300

    less7

---
+++ framework/templator.py
@@ -0,0 +1,37 @@
+# from jinja2 import Template
+# from pathlib import Path
+#
+# def render(template_name, **kwargs):
+#     """
+#     Минимальный пример работы с шаблонизатором
+#     :param template_name: имя шаблона
+#     :param kwargs: параметры для передачи в шаблон
+#     :return:
+#     """
+#
+#     file_path = Path.cwd() / 'templates' / template_name
+#     with open(file_path, encoding='utf-8') as f:
+#         template = Template(f.read())
+#
+#     return template.render(**kwargs)
+#
+
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+
+
+def render(template_name, folder='templates', **kwargs):
+    """
+    Минимальный пример работы с шаблонизатором
+    :param template_name: имя шаблона
+    :param kwargs: параметры для передачи в шаблон
+    :return:
+    """
+
+    # создаем объект окружения
+    env = Environment()
+    # указываем папку для поиска шаблонов
+    env.loader = FileSystemLoader(folder)
+    # находим шаблон в окружении
+    template = env.get_template(template_name)
+    return template.render(**kwargs)
